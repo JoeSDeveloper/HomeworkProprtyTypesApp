4b0eacab58518d475525d8bfb130d6f304057121
devel <devel@LAPTOP-MAD5555M>
2022-04-21 22:05:28 -0400

    Add project files.

---
+++ HomeworkProprtyTypes/ClientAddress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkProprtyTypes
+{
+    public class ClientAddress
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+
+        public string FullAddress
+        {
+            get
+            {
+                return $"{StreetAddress} {City} {State} {ZipCode}";
+            }
+             
+        }
+
+       
+    }
+}
---
+++ HomeworkProprtyTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkProprtyTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserMessages.WelcomeMessage("Welcome to the client address collection application");
+
+            ClientAddress address = new ClientAddress();
+            Console.Write("What street do you live on: ");
+            address.StreetAddress = Console.ReadLine();
+            Console.Write("What city do you live in: ");
+            address.City = Console.ReadLine();
+            Console.Write("What state do you live in: ");
+            address.State = Console.ReadLine();
+            Console.Write("What is your zip code: ");
+            address.ZipCode = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(address.FullAddress);
+
+
+            // This work but I need to add program functionality to it, so can get a person name, and also test the zipcode
+            Console.ReadLine();
+        }
+    }
+}
